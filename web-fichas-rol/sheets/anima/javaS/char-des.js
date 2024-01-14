da12f3f9ca78d69ab1f9b724fd9ce0eada06caef
Jonás J <62885155+Pretherox@users.noreply.github.com>
2024-01-14 23:45:24 +0100

    Social Class options under work
    Advances had been made and now it is possible for the usser to select his Social Class depending on his region
    (There are still a lot of options missing and I need to the program to delete the options before changing region again)

--- web-fichas-rol/sheets/anima/javaS/char-des.js
+++ web-fichas-rol/sheets/anima/javaS/char-des.js
@@ -1,6 +1,72 @@
-const region = document.getElementById('region');
-const value = region.value;
+const regionDropdown = document.querySelector('#region');
 
-if (region == 'ssi') {
-    
+regionDropdown.addEventListener('select', dropdownChange);
+
+//=== Option creator ===//
+const socialClass = document.getElementById('socialclass');
+
+function createOption(element,value,text){
+  const opt = document.createElement("option");
+
+  opt.value = value;
+  opt.text = text;
+
+  element.add(opt, null);
+}
+//=== Option deleter ===//
+
+
+// functiont to unhidde elements that must stay hidden until active
+function unhide(element){
+  document.getElementById(element).style.display = 'block';
+}
+
+function dropdownChange() {
+  console.log(regionDropdown.value);
+
+//===Sacro Santo Imperio de Abel===//
+if (regionDropdown.value == 'abel'){
+  unhide('regiondiv');
+
+  createOption(socialClass,'lowC','Clase Baja');
+  createOption(socialClass,'medC','Clase Media');
+  createOption(socialClass,'bourgeois','Burgués');
+  createOption(socialClass,'nobility','Nobleza');
+  createOption(socialClass,'special','Alta Nobleza');
+  }
+
+else if (regionDropdown.value == 'ilmora' || 'dalaborn'){
+  unhide('regiondiv');
+  
+  createOption(socialClass,'lowC','Clase Baja');
+  createOption(socialClass,'medC','Clase Media');
+  createOption(socialClass,'nobility','Nobleza');
+  createOption(socialClass,'special','Alta Nobleza');
+    }
+
+else if (regionDropdown.value == 'helenia'){
+  unhide('regiondiv');
+  
+  createOption(socialClass,'lowC','Clase Baja');
+  createOption(socialClass,'medC','Clase Media');
+  createOption(socialClass,'nomad','Nómada');
+  createOption(socialClass,'nobility','Nobleza');
+    }
+
+else if (regionDropdown.value == 'alberia' || 'galgados' || 'kanon'){
+  unhide('regiondiv');
+  
+  createOption(socialClass,'lowC','Clase Baja');
+  createOption(socialClass,'medC','Clase Media');
+  createOption(socialClass,'nobility','Nobleza');
+    }
+
+else if (regionDropdown.value == 'arlan'){
+  unhide('regiondiv');
+  
+  createOption(socialClass,'lowC','Clase Baja');
+  createOption(socialClass,'medC','Clase Media');
+  createOption(socialClass,'bourgeois','Burgués');
+  createOption(socialClass,'special','Alta Burguesía');
+    } 
 }
